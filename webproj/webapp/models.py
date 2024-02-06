669811e9ae4ef5ebf950d639b197de8baf4595ca
Gelix1470 <gleb.nazarov.1982@mail,ru>
2024-02-06 22:01:48 +0300

    dz2

--- webproj/webapp/models.py
+++ webproj/webapp/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Client(models.Model):
+    client_name = models.CharField(max_length=100, null=False)
+    email = models.EmailField()
+    password = models.CharField(max_length=100, null=False)
+    phone = models.CharField(max_length=50, null=True)
+    address = models.TextField(null=True)
+    reg_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.client_name}, email. {self.email}'
+
+    class Meta:
+        ordering = ['client_name']
+        indexes = [
+            models.Index(fields=['client_name']),
+            models.Index(fields=['-reg_date']),
+        ]
+
+
+class Product(models.Model):
+    prod_name = models.CharField(max_length=250, null=False)
+    description = models.TextField(null=True)
+    cost = models.DecimalField(max_digits=8, decimal_places=2, default=0, validators=[MinValueValidator(0)])
+    prod_count = models.IntegerField(default=0, validators=[MinValueValidator(0)])
+    append_date = models.DateTimeField(auto_now=True) 
+    def __str__(self):
+        return f'{self.prod_name}, наличие: {self.prod_count}, цена: {self.cost}'
+
+    class Meta:
+        ordering = ['prod_name']
+        indexes = [
+            models.Index(fields=['prod_name']),
+            models.Index(fields=['-append_date']),
+            models.Index(fields=['cost']),
+        ]
+
+
+class Order(models.Model):
+    order_date = models.DateField(auto_now_add=True)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0, validators=[MinValueValidator(0)])
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return f'{self.pk} - {self.order_date}'
--- webproj/webapp/views.py
+++ webproj/webapp/views.py
@@ -1,24 +1,48 @@
 from django.shortcuts import render
 import logging
-
+from django.http import HttpResponse
+from webapp.models import Client
 # Настраиваем логирование
 logger = logging.getLogger(__name__)
 
 def index(request):
-    html = """
-    <h1>Мой первый Django сайт!</h1>
-    <p>Меня зовут Глеб</p>
-    <p>На этом сайте я буду делиться своими проектами и опытом в программировании.</p>
-    """
-    logging.info(f"Пользователь посетил страницу 'главная'")
-    return render(request, 'index.html', {'html': html})
+    html = ("<a href='/'>Home</a>&nbsp;&nbsp;"
+            "<a href='/about/'>About</a>&nbsp;&nbsp;"
+            "<a href='/clients'>Clients</a>"
+            "<h1>Мой первый Django сайт!</h1>"
+            "<p>Меня зовут Глеб</p>"
+            "<p>На этом сайте я буду делиться своими проектами и опытом в программировании.</p>"
+            )
+    return HttpResponse(html)
 
 
 def about(request):
-    html = """
-    <h1>Обо мне</h1>
-    <p>Что то обо мне:</p>
-    <p>Я увлекаюсь созданием сайтов и приложений, особенно с использованием Django.</p>
-    """
-    logging.info(f"Пользователь посетил страницу 'о себе'")
-    return render(request, 'about.html', {'html': html})
+    html = ("<a href='/'>Home</a>&nbsp;&nbsp;"
+            "<a href='/about/'>About</a>&nbsp;&nbsp;"
+            "<a href='/clients'>Clients</a>"
+            "<h1>Обо мне<</h2>"
+            "<h3>Что то обо мне:</h3>"
+            "<p>Я увлекаюсь созданием сайтов и приложений, особенно с использованием Django.</p>"
+            )
+    return HttpResponse(html)
+
+
+def clients_list(request):
+    clients = Client.objects.all()
+
+    html = ("<a href='/'>Home</a>&nbsp;&nbsp;"
+            "<a href='/about/'>About</a>&nbsp;&nbsp;"
+            "<a href='/clients'>Clients</a>"
+            "<h1>Clients list</h2>"
+            "<table>"
+            "<tr><td>Name</td><td>E-mail</td><td>Phone</td><td>Address</td></tr>"
+            )
+    for client in clients:
+        html += ("<tr>"
+                 f"<td>{client.client_name}</td>"
+                 f"<td>{client.email}</td>"
+                 f"<td>{client.phone}</td>"
+                 f"<td>{client.address}</td>"
+                 "</tr>")
+    html += "</table>"
+    return HttpResponse(html)
